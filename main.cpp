0869016d6ce17ce05588c448deab2a7a65f90148
sleepysleepysleepyhead <hoeinsideme@gmail.com>
2026-01-28 22:05:41 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+
+    cout << "Enter a number: ";
+    cin >> number;
+
+    if (number % 2 == 0) {
+        cout << number << " is an even number" << endl;
+    } else {
+        cout << number << " is an odd number" << endl;
+    }
+
+    return 0;
+}
