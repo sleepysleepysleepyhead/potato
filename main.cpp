d8f61cc4ff58a8e301514d9fbfac2b1c303c9768
sleepysleepysleepyhead <hoeinsideme@gmail.com>
2026-01-28 22:07:08 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,10 +7,20 @@ int main() {
     cout << "Enter a number: ";
     cin >> number;
 
+    // Check even or odd
     if (number % 2 == 0) {
         cout << number << " is an even number" << endl;
     } else {
         cout << number << " is an odd number" << endl;
+    }
+
+    // Check positive, negative, or zero
+    if (number > 0) {
+        cout << number << " is a positive number" << endl;
+    } else if (number < 0) {
+        cout << number << " is a negative number" << endl;
+    } else {
+        cout << "The number is zero" << endl;
     }
 
     return 0;
